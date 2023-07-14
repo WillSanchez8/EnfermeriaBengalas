f31421dbc2cf0a8d880d1b4b1d29718e8214668b
WILLIAM SÁNCHEZ <william_as@tesch.edu.mx>
2023-07-13 20:36:58 -0600

    login listo, aun falta cargo

--- app/src/main/java/com/example/enfermeriabengalas/fragments/SignInFragment.kt
+++ app/src/main/java/com/example/enfermeriabengalas/fragments/SignInFragment.kt
@@ -1,60 +1,126 @@
 package com.example.enfermeriabengalas.fragments
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.Editable
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.TextWatcher
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.enfermeriabengalas.R
+import com.example.enfermeriabengalas.databinding.FragmentSignInBinding
+import com.example.enfermeriabengalas.databinding.FragmentSignUpBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SignInFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignInFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var auth: FirebaseAuth
+    private lateinit var navControl: NavController
+    private lateinit var binding: FragmentSignInBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+        binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignInFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignInFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+        registerEvents()
+
+        // Agregar TextWatchers a los campos de texto
+        binding.etEmailInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                validateField(s.toString(), binding.tilEmailInput)
+            }
+        })
+
+        binding.etPasswordInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                validateField(s.toString(), binding.tilPasswordInput)
+            }
+        })
+    }
+
+    private fun init(view: View) {
+        navControl = Navigation.findNavController(view)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun registerEvents() {
+        binding.authTextView.setOnClickListener {
+            navControl.navigate(R.id.action_signInFragment_to_signUpFragment)
+        }
+
+        binding.btnRegister.setOnClickListener {
+            val email = binding.etEmailInput.text.toString()
+            val pass = binding.etPasswordInput.text.toString()
+
+            // Validar si los campos están vacíos
+            var isValid = true
+            isValid = validateField(email, binding.tilEmailInput) && isValid
+            isValid = validateField(pass, binding.tilPasswordInput) && isValid
+
+            if (isValid) {
+                auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener { task ->
+                    view?.let { contextView ->
+                        if (task.isSuccessful) {
+                            Snackbar.make(contextView, "Sesión iniciada correctamente", Snackbar.LENGTH_SHORT).show()
+                            navControl.navigate(R.id.action_signInFragment_to_homeFragment)
+                        } else {
+                            if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                                // El correo electrónico o la contraseña son incorrectos
+                                val snackbarText = SpannableStringBuilder("El correo o la contraseña son inválidos, inténtalo nuevamente")
+                                snackbarText.setSpan(ForegroundColorSpan(Color.WHITE), 0, snackbarText.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                                snackbarText.setSpan(StyleSpan(Typeface.BOLD), 0, snackbarText.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                                Snackbar.make(contextView, snackbarText, Snackbar.LENGTH_SHORT).setBackgroundTint(Color.RED).show()
+                            } else {
+                                Snackbar.make(contextView, task.exception?.message ?: "", Snackbar.LENGTH_SHORT).show()
+                            }
+                        }
+                    } ?: run {
+                        // La vista es nula, el fragmento ya no está asociado a una actividad, no se puede mostrar un Snackbar
+                        // Mostrar un mensaje en el registro de la aplicación
+                        Log.w("SignInFragment", "No se puede mostrar el Snackbar porque la vista es nula")
+                    }
                 }
             }
+        }
+    }
+
+    fun validateField(field: String, inputLayout: TextInputLayout): Boolean {
+        return if (field.isEmpty()) {
+            inputLayout.setError("Este campo no debe estar vacío")
+            false
+        } else {
+            inputLayout.setError(null)
+            true
+        }
     }
 }
--- app/src/main/java/com/example/enfermeriabengalas/fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/enfermeriabengalas/fragments/SignUpFragment.kt
@@ -1,20 +1,181 @@
 package com.example.enfermeriabengalas.fragments
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.Editable
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.TextWatcher
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.enfermeriabengalas.R
+import com.example.enfermeriabengalas.databinding.FragmentSignUpBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
 
 class SignUpFragment : Fragment() {
 
+    private lateinit var auth:FirebaseAuth
+    private lateinit var navControl: NavController
+    private lateinit var binding: FragmentSignUpBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        binding = FragmentSignUpBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+        registerEvents()
+
+        // Agregar TextWatchers a los campos de texto
+        binding.etNameInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                validateField(s.toString(), binding.tilNameInput)
+            }
+        })
+
+        binding.etEmailInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                validateField(s.toString(), binding.tilEmailInput)
+            }
+        })
+
+        binding.etPasswordInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                validateField(s.toString(), binding.tilPasswordInput)
+            }
+        })
+
+        binding.etRepeatPasswordInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                validateField(s.toString(), binding.tilRepeatPasswordInput)
+            }
+        })
+    }
+
+    private fun init(view: View){
+        navControl = Navigation.findNavController(view)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun showErrorSnackbar(message: String) {
+        val contextView = view
+        if (contextView != null) {
+            val snackbarText = SpannableStringBuilder(message)
+            snackbarText.setSpan(ForegroundColorSpan(Color.WHITE), 0, snackbarText.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            snackbarText.setSpan(StyleSpan(Typeface.BOLD), 0, snackbarText.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+            Snackbar.make(contextView, snackbarText, Snackbar.LENGTH_SHORT).setBackgroundTint(Color.RED).show()
+        }
+    }
+
+    fun validateField(field: String, inputLayout: TextInputLayout): Boolean {
+        return if (field.isEmpty()) {
+            inputLayout.setError("Este campo no debe estar vacío")
+            false
+        } else {
+            inputLayout.setError(null)
+            true
+        }
+    }
+    private fun registerEvents() {
+        binding.authTextView.setOnClickListener {
+            navControl.navigate(R.id.action_signUpFragment_to_signInFragment)
+        }
+
+        binding.btnRegister.setOnClickListener {
+            val name = binding.etNameInput.text.toString()
+            val email = binding.etEmailInput.text.toString()
+            val pass = binding.etPasswordInput.text.toString()
+            val verifyPass = binding.etRepeatPasswordInput.text.toString().trim() //trim() elimina espacios en blanco
+            val cargo = binding.spinnerCargo.selectedItem.toString()
+
+            // Validar si los campos están vacíos
+            var isValid = true
+            isValid = validateField(name, binding.tilNameInput) && isValid
+            isValid = validateField(email, binding.tilEmailInput) && isValid
+            isValid = validateField(pass, binding.tilPasswordInput) && isValid
+            isValid = validateField(verifyPass, binding.tilRepeatPasswordInput) && isValid
+
+            // Validar si la contraseña tiene al menos 8 caracteres
+            if (isValid) {
+                // Validar si la contraseña tiene al menos 8 caracteres
+                if (pass.length < 8) {
+                    showErrorSnackbar("La contraseña debe tener al menos 8 caracteres")
+                    isValid = false
+                }
+
+                // Validar si las contraseñas coinciden
+                if (pass != verifyPass) {
+                    showErrorSnackbar("Las contraseñas no coinciden")
+                    isValid = false
+                }
+            }
+
+            if (isValid) {
+                view?.let { contextView ->
+                    auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            /* Guardar el nombre y el cargo del usuario en la base de datos
+                            val uid = auth.currentUser?.uid
+                            if (uid != null) {
+                                val database = FirebaseDatabase.getInstance()
+                                val ref = database.getReference("users").child(uid)
+                                ref.child("name").setValue(name)
+                                ref.child("cargo").setValue(cargo)
+                            }*/
+                            Snackbar.make(contextView, "Registro exitoso", Snackbar.LENGTH_SHORT).show()
+                            navControl.navigate(R.id.action_signUpFragment_to_homeFragment)
+                        } else {
+                            when (task.exception) {
+                                is FirebaseAuthUserCollisionException -> {
+                                    // El correo electrónico ya está asociado a una cuenta existente
+                                    showErrorSnackbar("Este correo ya está asociado, inicie sesión o inténtelo nuevamente")
+                                }
+                                else -> {
+                                    Snackbar.make(contextView, task.exception?.message ?: "Ocurrió un error", Snackbar.LENGTH_SHORT).show()
+                                }
+                            }
+                        }
+                    }
+                } ?: run {
+                    // La vista es nula, el fragmento ya no está asociado a una actividad, no se puede mostrar un Snackbar
+                    // Mostrar un mensaje en el registro de la aplicación
+                    Log.w("SignUpFragment", "No se puede mostrar el Snackbar porque la vista es nula")
+                }
+            }
+        }
+    }
 }
