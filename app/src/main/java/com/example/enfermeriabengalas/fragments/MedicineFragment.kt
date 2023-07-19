96ea1205e4745d3f2394c7a79c42ec7d26e7081e
WILLIAM SÁNCHEZ <william_as@tesch.edu.mx>
2023-07-18 19:27:47 -0600

    ya solo falta mostrar medicamentos en cardview, eliminar con dialogo y actualizar reciclando la vista de agregar medicamento

--- app/src/main/java/com/example/enfermeriabengalas/fragments/MedicineFragment.kt
+++ app/src/main/java/com/example/enfermeriabengalas/fragments/MedicineFragment.kt
@@ -5,16 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.enfermeriabengalas.R
+import com.example.enfermeriabengalas.databinding.FragmentMedicineBinding
+import com.example.enfermeriabengalas.viewmodel.MedicineViewModel
 
 class MedicineFragment : Fragment() {
+
+    private lateinit var binding: FragmentMedicineBinding
+    private lateinit var viewModel: MedicineViewModel
+    private lateinit var navControl: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_medicine, container, false)
+        binding= FragmentMedicineBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(requireActivity()).get(MedicineViewModel::class.java)
+        init(view)
+        registerEvents()
+
+        // Crea y establece un adaptador para el RecyclerView
+      /*  val adapter = MyAdapter()
+        binding.medicinesRecyclerView.adapter = adapter*/
+    }
+
+    private fun init(view: View) {
+        viewModel.selectedCategoryTitle.observe(viewLifecycleOwner) { title ->
+            // Reemplaza los saltos de línea con espacios
+            val formattedTitle = title.replace("\n", " ")
+            // Actualiza la interfaz de usuario con el título de la categoría seleccionada
+            binding.categoryTitle.text = formattedTitle
+        }
+        navControl = Navigation.findNavController(view)
+    }
+
+    private fun registerEvents() {
+        binding.backButton.setOnClickListener {
+            navControl.navigate(R.id.action_medicineFragment_to_homeFragment)
+        }
     }
 
 }
